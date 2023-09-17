da19d7cdf629b60bd0c3a010b3c13006a1a997df
Vitaliy Bobrovskiy <cool.rinet@yandex.ru>
2023-09-17 20:42:33 +0700

    bugfixed CheatActivity UI state

--- app/src/main/java/com/coolrinet/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/coolrinet/geoquiz/CheatActivity.kt
@@ -10,11 +10,13 @@ import com.coolrinet.geoquiz.databinding.ActivityCheatBinding
 const val EXTRA_ANSWER_SHOWN = "com.coolrinet.geoquiz.answer_shown"
 private const val EXTRA_ANSWER_IS_TRUE =
     "com.coolrinet.geoquiz.answer_is_true"
+private const val ANSWER_TEXT_KEY = "ANSWER_TEXT_KEY"
 
 class CheatActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCheatBinding
 
     private var answerIsTrue = false
+    private var answerText: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +26,25 @@ class CheatActivity : AppCompatActivity() {
         answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
 
         binding.showAnswerButton.setOnClickListener {
-            val answerText = when {
-                answerIsTrue -> R.string.true_button
-                else -> R.string.false_button
+            answerText = when {
+                answerIsTrue -> getString(R.string.true_button)
+                else -> getString(R.string.false_button)
             }
-            binding.answerTextView.setText(answerText)
+            binding.answerTextView.text = answerText
+            setAnswerShownResult(true)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(ANSWER_TEXT_KEY, answerText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        answerText = savedInstanceState.getString(ANSWER_TEXT_KEY)
+        if (answerText != null) {
+            binding.answerTextView.text = answerText
             setAnswerShownResult(true)
         }
     }
